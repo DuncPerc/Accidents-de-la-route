f1945b29fe95cae0386d598e51f8d88c83f28acc
Bocoum1 <bocoumoumar535@gmail.com>
2026-03-06 03:08:13 +0100

    Update global.R

--- global.R
+++ global.R
@@ -26,3 +26,41 @@ df <- read_excel("Data/Road_Accident_Data.xlsx") %>%
       "Total" = "Total"
     )
   )
+
+# Traduction des conditions d'éclairage en français
+df$Light_Conditions <- dplyr::recode(
+  df$Light_Conditions,
+  "Daylight" = "Jour",
+  "Darkness - lights lit" = "Nuit – éclairage allumé",
+  "Darkness - lights unlit" = "Nuit – éclairage éteint",
+  "Darkness - no lighting" = "Nuit – sans éclairage",
+  "Darkness - lighting unknown" = "Nuit – éclairage inconnu"
+)
+# Conversion de certaines variables en facteurs (variables catégorielles)
+
+df <- df %>%
+  mutate(across(
+    c(
+      Accident_Severity,
+      Road_Type,
+      Weather_Conditions,
+      Light_Conditions,
+      Road_Surface_Conditions,
+      Urban_or_Rural_Area
+    ),
+    as.factor
+  ))
+
+# Traitement des valeurs manquantes pour certaines variables catégorielles.
+# Les valeurs NA sont remplacées par une catégorie explicite afin d'éviter
+# la perte d'informations dans les analyses et graphiques.
+df <- df %>%
+  mutate(
+    Carriageway_Hazards = fct_na_value_to_level(Carriageway_Hazards, "None"),
+    Weather_Conditions = fct_na_value_to_level(Weather_Conditions, "Unknown"),
+    Road_Type = fct_na_value_to_level(Road_Type, "Unknown"),
+    Road_Surface_Conditions = fct_na_value_to_level(Road_Surface_Conditions, "Unknown")
+  ) %>%
+  # Suppression des observations pour lesquelles l'heure de l'accident est inconnue.
+  # Ces cas représentent seulement 0,0055 % du dataset
+  filter(!is.na(Time))
